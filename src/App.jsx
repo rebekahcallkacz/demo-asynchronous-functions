bc3822695f2a1b0d83a27992604bfc16fef7bd4c
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-11-21 20:08:48 -0500

    using a promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // Set this flag to "fakeError" to see what happens when there's an error
-  const flag = "noError";
+  const [isFakingError, setIsFakingError] = useState(true);
+  const [delayedData, setDelayedData] = useState({
+    isLoading: false,
+    isErrored: false,
+    data: undefined,
+  });
 
   // Create a promise that uses `setTimeout`
-  const getDataWithDelay = new Promise((resolve, reject) => {
+  const dataWithDelayPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (flag !== "fakeError") {
+      if (!isFakingError) {
         resolve("This is my successfully resolved promise!");
       } else {
         reject("My promise was rejected and there is an error");
       }
     }, 2000);
   });
+
+  const getDataWithDelay = () => {
+    const initialData = delayedData.data;
+    setDelayedData({ data: initialData, isLoading: true });
+    dataWithDelayPromise
+      .then((data) =>
+        setDelayedData({ data: data, isLoading: false, isErrored: false })
+      )
+      .catch((error) => {
+        console.log(error);
+        setDelayedData({ data: null, isLoading: false, isErrored: true });
+      });
+  };
+
+  useEffect(() => {
+    getDataWithDelay();
+  }, []);
+
   // Step 1. Get the data only on initial render and console.log it
   // Step 2. Get the data only on initial render and store it in local state
   // Step 3. Once the data is available, display it
   // Step 4. Add a loading indicator
   // Step 5. Handle errors
-  // Step 6. Trigger getting the data with the button instead of it happening automatically
+  // Step 6. Trigger getting the data again with the button
   // Step 7. Add a toggle for whether you want the flag to be noError or fakeError
 
   return (
     <>
       <h1>Asynchronous Functions Demo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        {isFakingError && <p>We are currently faking an error.</p>}
+        <button
+          onClick={() => {
+            setIsFakingError(!isFakingError);
+          }}
+        >
+          {isFakingError ? "Remove the Error" : "Fake an Error"}
         </button>
-        <p>Add conditionally rendered data here from promise.</p>
+        <button
+          disabled={delayedData.isLoading}
+          onClick={() => {
+            getDataWithDelay();
+          }}
+        >
+          Get the data again!
+        </button>
+        {delayedData.isLoading && <p>The data is loading...</p>}
+        {delayedData.data && <p>{delayedData.data}</p>}
+        {delayedData.isErrored && (
+          <p>We tried to get the data but there was an error :(</p>
+        )}
       </div>
     </>
   );
